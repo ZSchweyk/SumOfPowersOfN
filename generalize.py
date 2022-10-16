7c254ccc5f2b4f8a717de42fc913b4fba0ea299a
ZSchweyk <zeynschweyk@dpengineering.org>
2022-10-16 16:30:13 -0400

    got it working

--- generalize.py
+++ generalize.py
@@ -1,14 +1,14 @@
 def repr_as_sum_of_pow_of(num, base):
     array = []
-    num_copys = [num]
-    while num_copys[-1] != 0:
+    portion_of_num = num
+    while portion_of_num != 0:
         power = 0
-        num_copy = num_copys[-1]
+        num_copy = portion_of_num
         while num_copy >= base:
             num_copy = int(num_copy / base)
             power += 1
         array.append((num_copy, base, power))
-        num_copys.append(num_copys[-1] - (num_copy * (base) ** power))
+        portion_of_num = portion_of_num - (num_copy * base ** power)
 
     return print_as_sum_of_powers_of(array)
 
@@ -25,7 +25,7 @@ def print_as_sum_of_powers_of(array):
     return result
 
 
-s = repr_as_sum_of_pow_of(275, 10)
+s = repr_as_sum_of_pow_of(23, 3)
 print(s)
 
 
