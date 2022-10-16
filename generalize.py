f6df584c6866f59e2a5497006518db7c25e3cdc9
ZSchweyk <zeynschweyk@dpengineering.org>
2022-10-16 16:23:02 -0400

    works! but now I don't want to save the steps in a list

--- generalize.py
+++ generalize.py
@@ -1,13 +1,15 @@
 def repr_as_sum_of_pow_of(num, base):
     array = []
-    num_copy = num
-    while num_copy != 0:
+    num_copys = [num]
+    while num_copys[-1] != 0:
         power = 0
+        num_copy = num_copys[-1]
         while num_copy >= base:
             num_copy = int(num_copy / base)
             power += 1
         array.append((num_copy, base, power))
-        num_copy = num - (num_copy * (base) ** power)
+        num_copys.append(num_copys[-1] - (num_copy * (base) ** power))
+
     return print_as_sum_of_powers_of(array)
 
 
