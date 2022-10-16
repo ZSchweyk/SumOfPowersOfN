77e988bea32f18e02024a17452e244daf43fa038
ZSchweyk <zeynschweyk@dpengineering.org>
2022-10-16 17:07:43 -0400

    started adding comments to base_n.py

--- base_n.py
+++ base_n.py
@@ -1,8 +1,10 @@
 def repr_as_sum_of_pow_of(num: int, base: int):
-    array = []
-    portion_of_num = num
-    while portion_of_num != 0:
-        power = 0
+    array = []  # save multipliers, bases, and powers respectively in a 2D array. I process the elements in this array
+    # later and in a separate function.
+    portion_of_num = num  # this is the portion of the number remaining after finding a multiple of the biggest unique
+    # power of {base}
+    while portion_of_num != 0:  # while there is still some remainder of the number that is not 0...
+        power = 0  # initiate a variable that represents the number of times {base} can be divided into {num}
         multiplier = portion_of_num
         while multiplier >= base:
             multiplier = multiplier // base
@@ -25,7 +27,7 @@ def print_as_sum_of_powers(array):
     return result
 
 
-s = repr_as_sum_of_pow_of(23, 7)
+s = repr_as_sum_of_pow_of(9999999, 2)
 print(s)
 
 
